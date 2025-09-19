1827ba56a12f9cc842b4f98044ca9043ee4c2bb1
Denick <catlynsmith036@gmail.com>
2025-09-19 05:00:34 +0300

    Cart

---
+++ data/cart.js
@@ -0,0 +1,13 @@
+//A cart is basically a list, and inside the list we have the product we want to byy, the quantinty and the price
+//In js, we can represent this as an array:Inside we have some objects where wach object has product and quantity.
+//const cart = [{
+//     product: 'Basketball',
+//     quantity: 1
+//    },
+//    {
+//    product: 'T-shirt,
+//    quantity: 2
+// }
+// ]
+
+
