500c10fae3040565b470162942e0d150392a7f30
Denick <catlynsmith036@gmail.com>
2025-09-19 04:59:15 +0300

    Cart

--- app/static/js/amazon.js
+++ app/static/js/amazon.js
@@ -107,3 +107,17 @@ document.querySelectorAll('.js-add-to-cart')
         console.log('Added product')
     })
 })
+
+//A cart is basically a list, and inside the list we have the product we want to byy, the quantinty and the price
+//In js, we can represent this as an array:Inside we have some objects where wach object has product and quantity.
+//const cart = [{
+//     product: 'Basketball',
+//     quantity: 1
+//    },
+//    {
+//    product: 'T-shirt,
+//    quantity: 1
+// }
+// ]
+
+
