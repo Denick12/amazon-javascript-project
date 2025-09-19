358be676b4246d71b7716ed218b87f47d56548bb
Denick <catlynsmith036@gmail.com>
2025-09-19 06:02:53 +0300

    cart quantity accumulation

--- app/static/js/amazon.js
+++ app/static/js/amazon.js
@@ -133,7 +133,11 @@ document.querySelectorAll('.js-add-to-cart')
                     quantity: 1
                 });
             }
-
+            let cartQuantity = 0;
+            cart.forEach((item) =>{
+                cartQuantity +=item.quantity;
+            })
+            console.log(cartQuantity);
             console.log(cart)
         })
     })
