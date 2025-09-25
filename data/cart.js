1deaa26d15b15cffaa9ef44afd3c6cc974e02d53
Denick <catlynsmith036@gmail.com>
2025-09-25 05:16:23 +0300

    commit

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-//A cart is basically a list, and inside the list we have the product we want to byy, the quantinty and the price
+//A cart is basically a list, and inside the list we have the product we want to byy, the quantity and the price
 //In js, we can represent this as an array:Inside we have some objects where wach object has product and quantity.
 //const cart = [{
 //     product: 'Basketball',
