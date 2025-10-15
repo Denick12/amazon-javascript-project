4d8af29aa82127e4e27db5b80a1e5a4ff6e592a6
Denick <catlynsmith036@gmail.com>
2025-10-15 05:31:03 +0300

    adding event listener to delete

--- app/static/js/checkout.js
+++ app/static/js/checkout.js
@@ -41,7 +41,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link">
                     Delete
                   </span>
                 </div>
@@ -170,4 +170,12 @@ cart.forEach((cartItem) => {
     `;
     document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
     console.log(cartSummaryHTML)
+
+    document.querySelectorAll('.js-delete-link')
+        .forEach((link)=>{
+            link.addEventListener('click', ()=>{
+                console.log('delete');
+            })
+        })
+
 })
